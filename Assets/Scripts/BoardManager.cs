bd3f293bd023df3cd242a884084ed3aac1704b34
Daniel Martinez <dany_rolero@hotmail.com>
2025-02-12 21:22:41 +0100

    Tilemap lab

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardManager : MonoBehaviour
+{
+   public List<Tile> tiles;
+   public Tilemap tilemap;
+
+    private void Start() {
+        tilemap.SetTile(new Vector3Int(0, 0, 0), tiles[1]);
+        tilemap.SetTile(new Vector3Int(1, 1, 0), tiles[0]);
+        Debug.Log(tilemap.GetTile(new Vector3Int(0, 0, 0)).name);
+    }
+}
