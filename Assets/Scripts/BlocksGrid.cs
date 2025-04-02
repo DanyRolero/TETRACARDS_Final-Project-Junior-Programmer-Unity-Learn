65c0250e2d5341ac0ef16877649f7174efa4b920
Danito <dany_rolero@hotmail.com>
2025-04-02 16:44:39 +0200

    Update BlocksGrid class

--- Assets/Scripts/BlocksGrid.cs
+++ Assets/Scripts/BlocksGrid.cs
@@ -14,23 +14,33 @@ public class BlocksGrid : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------
-    public BlockData this[int x, int y] => blocks[x, y];
+    public BlockData this[int x, int y]
+    {
+        get 
+        {
+            CheckCellInBounds(x, y);
+            return blocks[x, y]; 
+        }
+    }
 
     //--------------------------------------------------------------------------------
     public BlockData GetBlock(Vector3Int cell)
     {
+        CheckCellInBounds(cell);
         return blocks[cell.x, cell.y];
     }
 
     //--------------------------------------------------------------------------------
     public void SetCell(Vector3Int cell, BlockData block)
     {
+        CheckCellInBounds(cell);
         blocks[cell.x, cell.y] = block;
     }
 
     //--------------------------------------------------------------------------------
     public void UnsetCell(Vector3Int cell)
     {
+        CheckCellInBounds(cell);
         blocks[cell.x, cell.y] = null;
     }
 
@@ -43,6 +53,7 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public bool IsCellOccupied(Vector3Int cell)
     {
+        CheckCellInBounds(cell);
         return blocks[cell.x, cell.y] != null;
     }
 
@@ -64,6 +75,7 @@ public class BlocksGrid : MonoBehaviour
     {
         foreach (Vector3Int cell in polyminoData.Cells)
         {
+            CheckCellInBounds(cell);
             if (IsCellOccupied(cell)) return true;
         }
 
@@ -73,6 +85,8 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public bool IsRowComplete(int row)
     {
+        CheckCellInBounds(0, row);
+
         for (int i = 0; i < blocks.GetLength(0); i++)
         {
             if (blocks[i, row] == null) return false;
@@ -84,6 +98,8 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public Vector3Int[] GetRow(int row)
     {
+        CheckCellInBounds(0, row);
+
         List<Vector3Int> rowPositions = new List<Vector3Int>();
 
         for (int x = 0; x < blocks.GetLength(0); x++)
@@ -102,6 +118,7 @@ public class BlocksGrid : MonoBehaviour
 
         foreach (Vector3Int position in blocksPositions)
         {
+            CheckCellInBounds(position);
             BlockData currentBlock = blocks[position.x, position.y];
             if (currentBlock == null || !currentBlock.Equals(sampleBlock))
             {
@@ -113,8 +130,10 @@ public class BlocksGrid : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------
-    public bool IsMonoTypeRow(int row)
+    public bool IsMonoTypeBlockRow(int row)
     {
+        CheckCellInBounds(0, row);
+
         BlockData sampleBlock = blocks[0, row];
 
         for (int x = 1; x < blocks.GetLength(0); x++)
@@ -131,8 +150,11 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public int CountSimilarsBlocksInBoard(Vector3Int blockPosition)
     {
+        CheckCellInBounds(blockPosition);
+
         int count = 0;
         BlockData sampleBlock = blocks[blockPosition.x, blockPosition.y];
+
         if (sampleBlock == null) return count;
         CellsIterate((x, y, block) =>
         {
@@ -146,9 +168,14 @@ public class BlocksGrid : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------
-    public Vector3Int[] GetSimilarsBlocksPositions(BlockData sampleBlock)
+    public Vector3Int[] GetSimilarsBlocksPositions(Vector3Int blockPosition)
     {
+        CheckCellInBounds(blockPosition);
+
         List<Vector3Int> positions = new List<Vector3Int>();
+        BlockData sampleBlock = blocks[blockPosition.x, blockPosition.y];
+
+        if (sampleBlock == null) return positions.ToArray();
 
         CellsIterate((x, y, block) =>
         {
@@ -162,13 +189,18 @@ public class BlocksGrid : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------
-    public bool IsNonRepatedTypeBlocks(BlockData[] blocks)
+    public bool IsNonRepeatedTypeBlockFullkRow(int row)
     {
+        CheckCellInBounds(0, row);
+
         HashSet<BlockData> uniqueBlocks = new HashSet<BlockData>();
 
-        foreach (BlockData block in blocks)
+        for (int x = 0; x < blocks.GetLength(0); x++)
         {
-            if (block != null && !uniqueBlocks.Add(block))
+            BlockData currentBlock = blocks[x, row];
+            if (currentBlock == null) return false;
+
+            if (!uniqueBlocks.Add(currentBlock))
             {
                 return false;
             }
@@ -180,6 +212,8 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public Vector3Int GetUppestCellUnsetPosition(int column)
     {
+        CheckCellInBounds(column, 0);
+
         for (int y = blocks.GetLength(1) - 1; y >= 0; y--)
         {
             if (blocks[column, y] == null)
@@ -191,8 +225,11 @@ public class BlocksGrid : MonoBehaviour
         return new Vector3Int(column, -1, 0);
     }
 
+    //--------------------------------------------------------------------------------
     public Vector3Int GetLowestCellUnsetPosition(int column)
     {
+        CheckCellInBounds(column, 0);
+
         for (int y = 0; y < blocks.GetLength(1); y++)
         {
             if (blocks[column, y] == null)
@@ -207,6 +244,8 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public bool IsColumnFull(int column)
     {
+        CheckCellInBounds(column, 0);
+
         for (int y = 0; y < blocks.GetLength(1); y++)
         {
             if (blocks[column, y] == null) return false;
@@ -216,20 +255,24 @@ public class BlocksGrid : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------
-    public BlockData[] GetColumn(int column)
+    public Vector3Int[] GetColumn(int column)
     {
-        List<BlockData> columnBlocks = new List<BlockData>();
+        CheckCellInBounds(column, 0);
+
+        List<Vector3Int> columnPositions = new List<Vector3Int>();
+
         for (int y = 0; y < blocks.GetLength(1); y++)
         {
-            columnBlocks.Add(blocks[column, y]);
+            columnPositions.Add(new Vector3Int(column, y, 0));
         }
-
-        return columnBlocks.ToArray();
+        return columnPositions.ToArray();
     }
 
     //--------------------------------------------------------------------------------
     public bool HasAdjacentBlock(Vector3Int cell, Vector3Int direction)
     {
+        CheckCellInBounds(cell);
+
         Vector3Int adjacentCell = cell + direction;
 
         if (!IsCellInBounds(adjacentCell))
@@ -243,6 +286,8 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public Vector3Int GetAdjacentBlockPosition(Vector3Int cell, Vector3Int direction)
     {
+        CheckCellInBounds(cell);
+
         Vector3Int adjacentCell = cell + direction;
 
         if (!IsCellInBounds(adjacentCell))
@@ -256,6 +301,7 @@ public class BlocksGrid : MonoBehaviour
     //--------------------------------------------------------------------------------
     public bool IsAdjacentSimilarBlock(Vector3Int cell, Vector3Int direction)
     {
+        CheckCellInBounds(cell);
         
         Vector3Int adjacentBlock = GetAdjacentBlockPosition(cell, direction);
         if (adjacentBlock == null) return false;
@@ -337,4 +383,41 @@ public class BlocksGrid : MonoBehaviour
             }
         }
     }
+
+    //--------------------------------------------------------------------------------
+    private void CheckCellInBounds(Vector3Int cell)
+    {
+        if (!IsCellInBounds(cell))
+        {
+            throw new ArgumentOutOfRangeException($"Cell is out of bounds x: {cell.x}, y:{cell.y}.");
+        }
+    }
+
+        //--------------------------------------------------------------------------------
+    private void CheckCellInBounds(int x, int y)
+    {
+        if (!IsCellInBounds(new Vector3Int(x, y, 0)))
+        {
+            throw new ArgumentOutOfRangeException($"Cell is out of bounds x: {x}, y:{y}.");
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    private void CheckPositionIsNull(Vector3Int cell)
+    {
+        if (blocks[cell.x, cell.y] == null)
+        {
+            throw new ArgumentNullException($"Block at position x: {cell.x}, y:{cell.y} is null.");
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    private void CheckPositionIsNull(int x, int y)
+    {
+        if (blocks[x, y] == null)
+        {
+            throw new ArgumentNullException($"Block at position x: {x}, y:{y} is null.");
+        }
+    }
+
 }
